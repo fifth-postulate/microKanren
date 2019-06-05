36e660bfee7b7dca69ed8acb3c5baa80cf11b603
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2019-06-05 21:02:45 +0200

    Walk a Term in a State

--- microKanren/src/main/java/fifth/postulate/kanren/State.java
+++ microKanren/src/main/java/fifth/postulate/kanren/State.java
@@ -1,7 +1,6 @@
 package fifth.postulate.kanren;
 
-import java.util.HashMap;
-import java.util.Map;
+import static fifth.postulate.kanren.Term.variable;
 
 /**
  * A state is a pair of a substitution and fresh variable
@@ -12,10 +11,86 @@ public class State<T> {
     }
 
     private final Variable fresh;
-    private final Map<Variable, Term<T>> substitution;
+    private final Substitution<T> substitution;
 
     private State() {
         this.fresh = Variable.first();
-        this.substitution = new HashMap();
+        this.substitution = Substitution.empty();
+    }
+
+    public Term<T> walk(Term<T> term) {
+        if (substitution.containsKey(term)) {
+            Term<T> association = substitution.get(term);
+            return walk(association);
+        } else {
+            return term;
+        }
     }
 }
+
+interface Substitution<T> {
+    static <V> Substitution<V> empty() {
+        return Empty.instance();
+    }
+
+    static <V> Substitution<V> extend(Variable variable, Term<V> term, Substitution<V> chain) {
+        return extend(variable, term, chain);
+    }
+
+    boolean containsKey(Term<T> term);
+
+    Term<T> get(Term<T> term);
+}
+
+class Association<T> implements Substitution {
+    public static <V> Substitution<V> extend(Variable variable, Term<V> term, Substitution<V> chain) {
+        return new Association(variable, term, chain);
+    }
+
+    private final Term<T> key;
+    private final Term<T> association;
+    private final Substitution<T> chain;
+
+    private Association(Variable variable, Term<T> term, Substitution<T> chain) {
+        this.key = variable(variable);
+        this.association = term;
+        this.chain = chain;
+    }
+
+    @Override
+    public boolean containsKey(Term term) {
+        return key.equals(term) || chain.containsKey(term);
+    }
+
+    @Override
+    public Term get(Term term) {
+        if (key.equals(term)) {
+            return association;
+        } else {
+            return chain.get(term);
+        }
+    }
+}
+
+class Empty<T> implements Substitution<T> {
+    private static Substitution instance;
+
+    public static <T> Substitution<T> instance() {
+        if (instance == null) {
+            instance = new Empty();
+        }
+        return (Substitution<T>) instance;
+    }
+
+    @Override
+    public boolean containsKey(Term<T> term) {
+        return false;
+    }
+
+    @Override
+    public Term<T> get(Term<T> term) {
+        return term;
+    }
+}
+
+
